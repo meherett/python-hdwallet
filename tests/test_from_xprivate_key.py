abd7210972c1c1ff0f95e6ecab7579d0224e9206
meherett <meherett@zoho.com>
2021-10-09 21:05:35 +0300

    Update: Removed root key testers and add more tests on from xprv and xpub keys.

--- tests/test_from_xprivate_key.py
+++ tests/test_from_xprivate_key.py
@@ -16,11 +16,72 @@ values.close()
 def test_from_xprivate_key():
 
     hdwallet: HDWallet = HDWallet(
+        symbol=_["bitcoin"]["mainnet"]["symbol"]
+    )
+    hdwallet.from_xprivate_key(
+        xprivate_key=_["bitcoin"]["mainnet"]["root_xprivate_key"], strict=True
+    )
+    hdwallet.from_path(
+        path=_["bitcoin"]["mainnet"]["path"]
+    )
+
+    assert hdwallet.cryptocurrency() == _["bitcoin"]["mainnet"]["cryptocurrency"]
+    assert hdwallet.symbol() == _["bitcoin"]["mainnet"]["symbol"]
+    assert hdwallet.network() == _["bitcoin"]["mainnet"]["network"]
+    assert hdwallet.strength() is None
+    assert hdwallet.entropy() is None
+    assert hdwallet.mnemonic() is None
+    assert hdwallet.language() is None
+    assert hdwallet.passphrase() is None
+    assert hdwallet.seed() is None
+    assert hdwallet.root_xprivate_key(encoded=False) == _["bitcoin"]["mainnet"]["root_xprivate_key_hex"]
+    assert hdwallet.root_xprivate_key() == _["bitcoin"]["mainnet"]["root_xprivate_key"]
+    assert hdwallet.root_xpublic_key(encoded=False) == _["bitcoin"]["mainnet"]["root_xpublic_key_hex"]
+    assert hdwallet.root_xpublic_key() == _["bitcoin"]["mainnet"]["root_xpublic_key"]
+    assert hdwallet.xprivate_key(encoded=False) == _["bitcoin"]["mainnet"]["xprivate_key_hex"]
+    assert hdwallet.xprivate_key() == _["bitcoin"]["mainnet"]["xprivate_key"]
+    assert hdwallet.xpublic_key(encoded=False) == _["bitcoin"]["mainnet"]["xpublic_key_hex"]
+    assert hdwallet.xpublic_key() == _["bitcoin"]["mainnet"]["xpublic_key"]
+    assert hdwallet.uncompressed() == _["bitcoin"]["mainnet"]["uncompressed"]
+    assert hdwallet.compressed() == _["bitcoin"]["mainnet"]["compressed"]
+    assert hdwallet.chain_code() == _["bitcoin"]["mainnet"]["chain_code"]
+    assert hdwallet.private_key() == _["bitcoin"]["mainnet"]["private_key"]
+    assert hdwallet.public_key() == _["bitcoin"]["mainnet"]["public_key"]
+    assert hdwallet.wif() == _["bitcoin"]["mainnet"]["wif"]
+    assert hdwallet.finger_print() == _["bitcoin"]["mainnet"]["finger_print"]
+    assert hdwallet.semantic() == _["bitcoin"]["mainnet"]["semantic"]
+    assert hdwallet.path() == _["bitcoin"]["mainnet"]["path"]
+    assert hdwallet.hash() == _["bitcoin"]["mainnet"]["hash"]
+    assert hdwallet.p2pkh_address() == _["bitcoin"]["mainnet"]["addresses"]["p2pkh"]
+    assert hdwallet.p2sh_address() == _["bitcoin"]["mainnet"]["addresses"]["p2sh"]
+    assert hdwallet.p2wpkh_address() == _["bitcoin"]["mainnet"]["addresses"]["p2wpkh"]
+    assert hdwallet.p2wpkh_in_p2sh_address() == _["bitcoin"]["mainnet"]["addresses"]["p2wpkh_in_p2sh"]
+    assert hdwallet.p2wsh_address() == _["bitcoin"]["mainnet"]["addresses"]["p2wsh"]
+    assert hdwallet.p2wsh_in_p2sh_address() == _["bitcoin"]["mainnet"]["addresses"]["p2wsh_in_p2sh"]
+
+    assert isinstance(hdwallet.dumps(), dict)
+
+    dumps: dict = _["bitcoin"]["mainnet"]
+
+    dumps["strength"] = None
+    dumps["entropy"] = None
+    dumps["mnemonic"] = None
+    dumps["language"] = None
+    dumps["passphrase"] = None
+    dumps["seed"] = None
+    del dumps["root_xprivate_key_hex"]
+    del dumps["root_xpublic_key_hex"]
+    del dumps["xprivate_key_hex"]
+    del dumps["xpublic_key_hex"]
+
+    assert hdwallet.dumps() == dumps
+
+    hdwallet: HDWallet = HDWallet(
         symbol=_["bitcoin"]["testnet"]["symbol"]
     )
-    
+
     hdwallet.from_xprivate_key(
-        xprivate_key=_["bitcoin"]["testnet"]["xprivate_key"]
+        xprivate_key=_["bitcoin"]["testnet"]["xprivate_key"], strict=False
     )
 
     assert hdwallet.cryptocurrency() == _["bitcoin"]["testnet"]["cryptocurrency"]
@@ -32,10 +93,10 @@ def test_from_xprivate_key():
     assert hdwallet.language() is None
     assert hdwallet.passphrase() is None
     assert hdwallet.seed() is None
-    assert hdwallet.root_xprivate_key(encoded=False) is None
-    assert hdwallet.root_xprivate_key() is None
-    assert hdwallet.root_xpublic_key(encoded=False) is None
-    assert hdwallet.root_xpublic_key() is None
+    assert hdwallet.root_xprivate_key(encoded=False) == _["bitcoin"]["testnet"]["xprivate_key_hex"]
+    assert hdwallet.root_xprivate_key() == _["bitcoin"]["testnet"]["xprivate_key"]
+    assert hdwallet.root_xpublic_key(encoded=False) == _["bitcoin"]["testnet"]["xpublic_key_hex"]
+    assert hdwallet.root_xpublic_key() == _["bitcoin"]["testnet"]["xpublic_key"]
     assert hdwallet.xprivate_key(encoded=False) == _["bitcoin"]["testnet"]["xprivate_key_hex"]
     assert hdwallet.xprivate_key() == _["bitcoin"]["testnet"]["xprivate_key"]
     assert hdwallet.xpublic_key(encoded=False) == _["bitcoin"]["testnet"]["xpublic_key_hex"]
@@ -67,9 +128,9 @@ def test_from_xprivate_key():
     dumps["language"] = None
     dumps["passphrase"] = None
     dumps["seed"] = None
-    dumps["root_xprivate_key"] = None
-    dumps["root_xpublic_key"] = None
     dumps["path"] = None
+    dumps["root_xprivate_key"] = _["bitcoin"]["testnet"]["xprivate_key"]
+    dumps["root_xpublic_key"] = _["bitcoin"]["testnet"]["xpublic_key"]
     del dumps["root_xprivate_key_hex"]
     del dumps["root_xpublic_key_hex"]
     del dumps["xprivate_key_hex"]
--- tests/test_from_xpublic_key.py
+++ tests/test_from_xpublic_key.py
@@ -16,11 +16,57 @@ values.close()
 def test_from_xpublic_key():
 
     hdwallet: HDWallet = HDWallet(
+        symbol=_["bitcoin"]["mainnet"]["symbol"]
+    )
+    hdwallet.from_xpublic_key(
+        xpublic_key=_["bitcoin"]["mainnet"]["root_xpublic_key"], strict=True
+    )
+    hdwallet.from_path(
+        path="m/44/0/0/0/0"
+    )
+
+    assert hdwallet.cryptocurrency() == _["bitcoin"]["mainnet"]["cryptocurrency"]
+    assert hdwallet.symbol() == _["bitcoin"]["mainnet"]["symbol"]
+    assert hdwallet.network() == _["bitcoin"]["mainnet"]["network"]
+    assert hdwallet.strength() is None
+    assert hdwallet.entropy() is None
+    assert hdwallet.mnemonic() is None
+    assert hdwallet.language() is None
+    assert hdwallet.passphrase() is None
+    assert hdwallet.seed() is None
+    assert hdwallet.root_xprivate_key(encoded=False) is None
+    assert hdwallet.root_xprivate_key() is None
+    assert hdwallet.root_xpublic_key(encoded=False) == "0488b21e000000000000000000ad41ef910cdcae932cb4060777b4284ee38f5b29c5fb60fda8416f298a14702c02949b9f64223e124eb9a8383fba0b21b5845fcfbdc84dec7692d21c716410eab0"
+    assert hdwallet.root_xpublic_key() == "xpub661MyMwAqRbcGGUtsoFw2d6ARvD2ABd7z327zxt2XiBBwMx9GAuNrrE7tbRuWF5MjjZ1BzDsRdaSHc9nVKAgHzQrv6pwYW3Hd7LSzbh8sWS"
+    assert hdwallet.xprivate_key(encoded=False) is None
+    assert hdwallet.xprivate_key() is None
+    assert hdwallet.xpublic_key(encoded=False) == "0488b21e052c0269af000000006c95c19e932b9e8f3d834e874526768ca1b3d89933ad71fd8253bcca67ac283d038f24175db513b40c75503c25040e5f0ea4d38e912d1f83daf5fd8c4b9512ad87"
+    assert hdwallet.xpublic_key() == "xpub6FjoSaU1JaG6fC6wTYmb1mJzaZxSunxASN7nTRHhFynh33gKRfmmNrtQ82s8YouLCrEniskjumfACiiTyVmi4aXyLL8HvLdZc8mjKsbzT9z"
+    assert hdwallet.uncompressed() == "8f24175db513b40c75503c25040e5f0ea4d38e912d1f83daf5fd8c4b9512ad8750a64d9e0ee3555225e4130c7e36a443ec20330bf0be1e4de913e31e00202993"
+    assert hdwallet.compressed() == "038f24175db513b40c75503c25040e5f0ea4d38e912d1f83daf5fd8c4b9512ad87"
+    assert hdwallet.chain_code() == "6c95c19e932b9e8f3d834e874526768ca1b3d89933ad71fd8253bcca67ac283d"
+    assert hdwallet.private_key() is None
+    assert hdwallet.public_key() == "038f24175db513b40c75503c25040e5f0ea4d38e912d1f83daf5fd8c4b9512ad87"
+    assert hdwallet.wif() is None
+    assert hdwallet.finger_print() == "4e749a26"
+    assert hdwallet.semantic() == "p2pkh"
+    assert hdwallet.path() == "m/44/0/0/0/0"
+    assert hdwallet.hash() == "4e749a26934bca5091a05ee6f55e7d0e21482647"
+    assert hdwallet.p2pkh_address() == "189qPd6J81ns9LEGx6kun7Xtg1bJV8GJXh"
+    assert hdwallet.p2sh_address() == "3C71bNRojv3Gc7zHvWygas4AFt34rKezcF"
+    assert hdwallet.p2wpkh_address() == "bc1qfe6f5f5nf099pydqtmn02hnapcs5sfj86dpqjm"
+    assert hdwallet.p2wpkh_in_p2sh_address() == "3NykoodgJ7Li43JPt5xsezQz8xfwwwFZUs"
+    assert hdwallet.p2wsh_address() == "bc1qazm6kznlgs06exh4cq2qxh567xrffppwujje5zg84upnng4essusd08nhz"
+    assert hdwallet.p2wsh_in_p2sh_address() == "32yGj8ncXBBTjXqg188ZHxd1xffoQDcjin"
+
+    assert isinstance(hdwallet.dumps(), dict)
+
+    hdwallet: HDWallet = HDWallet(
         symbol=_["bitcoin"]["testnet"]["symbol"]
     )
-    
+
     hdwallet.from_xpublic_key(
-        xpublic_key=_["bitcoin"]["testnet"]["xpublic_key"]
+        xpublic_key=_["bitcoin"]["testnet"]["xpublic_key"], strict=False
     )
 
     assert hdwallet.cryptocurrency() == _["bitcoin"]["testnet"]["cryptocurrency"]
@@ -34,8 +80,8 @@ def test_from_xpublic_key():
     assert hdwallet.seed() is None
     assert hdwallet.root_xprivate_key(encoded=False) is None
     assert hdwallet.root_xprivate_key() is None
-    assert hdwallet.root_xpublic_key(encoded=False) is None
-    assert hdwallet.root_xpublic_key() is None
+    assert hdwallet.root_xpublic_key(encoded=False) == _["bitcoin"]["testnet"]["xpublic_key_hex"]
+    assert hdwallet.root_xpublic_key() == _["bitcoin"]["testnet"]["xpublic_key"]
     assert hdwallet.xprivate_key(encoded=False) is None
     assert hdwallet.xprivate_key() is None
     assert hdwallet.xpublic_key(encoded=False) == _["bitcoin"]["testnet"]["xpublic_key_hex"]
@@ -70,11 +116,11 @@ def test_from_xpublic_key():
     dumps["passphrase"] = None
     dumps["seed"] = None
     dumps["root_xprivate_key"] = None
-    dumps["root_xpublic_key"] = None
     dumps["xprivate_key"] = None
     dumps["private_key"] = None
     dumps["wif"] = None
     dumps["path"] = None
+    dumps["root_xpublic_key"] = _["bitcoin"]["testnet"]["xpublic_key"]
     del dumps["root_xprivate_key_hex"]
     del dumps["root_xpublic_key_hex"]
     del dumps["xprivate_key_hex"]
