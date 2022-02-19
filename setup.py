d3b2da333f15595dfb9be44ecef3f999a57b8419
meherett <meherett@zoho.com>
2022-02-19 17:40:07 +0300

    Add: Extras CLI requirements and bump docs packages.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("requirements.txt", "r") as _requirements:
 
 setup(
     name="hdwallet",
-    version="v2.1.0",
+    version="v2.1.1",
     description="Python-based library for the implementation of a hierarchical deterministic wallet "
                 "generator for more than 140+ multiple cryptocurrencies.",
     long_description=long_description,
@@ -40,14 +40,19 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     extras_require={
+        "cli": [
+            "click>=8.0.3,<9",
+            "click-aliases>=1.0.1,<2",
+            "tabulate>=0.8.9,<1"
+        ],
         "tests": [
             "pytest>=6.2.5,<7",
             "pytest-cov>=3.0.0,<4"
         ],
         "docs": [
-            "sphinx>=4.3.2,<5",
-            "furo==2021.11.23",
-            "sphinx-click>=3.0.2,<4"
+            "sphinx>=4.4.0,<5",
+            "furo==2022.2.14.1",
+            "sphinx-click>=3.1.0,<4"
         ]
     },
     classifiers=[
