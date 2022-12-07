dfee315dc9e0249d562f5fe2ad8fb59f192e1dfc
meherett <meherett@zoho.com>
2022-12-07 17:47:12 +0300

    Bump: Python-HDWallet from `v2.1.2` to `v2.2.0` package.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("requirements.txt", "r") as _requirements:
 
 setup(
     name="hdwallet",
-    version="v2.1.2",
+    version="v2.2.0",
     description="Python-based library for the implementation of a hierarchical deterministic wallet "
                 "generator for more than 140+ multiple cryptocurrencies.",
     long_description=long_description,
