da3a3b8ab1747a9f20c700bd6904a6010c2db0ab
meherett <meherett@zoho.com>
2022-12-22 10:08:11 +0300

    Drop: default language param from mnemonic func for auto selection.

--- hdwallet/cli/generate/hdwallet.py
+++ hdwallet/cli/generate/hdwallet.py
@@ -47,7 +47,7 @@ def generate_hdwallet(
             )
         elif mnemonic:
             hdwallet.from_mnemonic(
-                mnemonic=mnemonic, language=language, passphrase=passphrase
+                mnemonic=mnemonic, passphrase=passphrase
             )
         elif seed:
             hdwallet.from_seed(
